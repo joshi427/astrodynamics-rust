67d6610e3ee9485ca5b714716063b270f4c1a682
joshi427 <124478494+joshi427@users.noreply.github.com>
2024-09-09 13:13:35 -0700

    created Vector3D impl operations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+#[derive(Debug, Clone, Copy)]
+struct Vector3D {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+impl Vector3D {
+    fn new(x: f64, y: f64, z: f64) -> Vector3D {
+        Vector3D { x, y, z}
+    }
+
+    fn add(self, vector: Vector3D) -> Vector3D {
+        Vector3D {
+            x: self.x + vector.x,
+            y: self.y + vector.y,
+            z: self.z + vector.z,
+        }
+    }
+
+    fn subtract(self, vector: Vector3D) -> Vector3D {
+        Vector3D {
+            x: self.x - vector.x,
+            y: self.y - vector.y,
+            z: self.z - vector.z,
+        }
+    }
+
+    fn scale(self, scalar: f64) -> Vector3D {
+        Vector3D {
+            x: self.x * scalar,
+            y: self.y * scalar,
+            z: self.z * scalar,
+        }
+    }
+
+    fn norm(&self) -> f64 {
+        (self.x * self.x + self.y * self.y + self.z * self.z).sqrt()
+    }
+
+    fn normalize(self) -> Vector3D {
+        let norm = self.norm();
+        Vector3D {
+            x: self.x / norm,
+            y: self.y / norm,
+            z: self.z / norm,
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
